1bce1820996bd78282a331ed6a95ce2f4cb5b51b
Andrew <55204554+Void-fun@users.noreply.github.com>
2021-04-07 00:03:40 +0300

    style

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,10 @@
-// Copyright 2021 asma
-#include "train.h"
+// Copyright 2021 ivanovd4
 #include <iostream>
+#include "train.h"
 
 int main() {
-	srand(time(NULL));
-	int len = 1000;
-	Train train(len);
-	std::cout << calculate_len(train.first) << len << std::endl;
-	return 0;
+  srand(time(NULL)); int len = 1000;
+  Train train(len);
+  std::cout << calculate_len(train.first) << len << std::endl;
+  return 0;
 }
